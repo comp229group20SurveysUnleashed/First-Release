c142824a386c12625c8485f380e24fbbd18becd5
Riccardo Reali <rreali@my.centennialcollege.ca>
2022-11-10 16:42:20 -0500

    First Organization Update

---
+++ app/routes/surveys.router.server.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { DisplaySurveyList } from "../controllers/surveys.controller.server.js";
+
+const router = Router();
+
+router.get('/survey-list', DisplaySurveyList);
+
+
+
+export default router;
