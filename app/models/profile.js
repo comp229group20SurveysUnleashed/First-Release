88fb8c69468467c510a9e997da9f9f21b3520858
Mohammad <mdsalam1994@hotmail.com>
2022-11-25 02:55:34 -0500

    Working on profile

---
+++ app/models/profile.js
@@ -0,0 +1,17 @@
+// import mongoose from "mongoose";
+// import passportLocalMongoose from 'passport-local-mongoose';
+// const { PassportLocalSchema } = mongoose;
+// const Schema = mongoose.Schema;
+
+// const ProfileSchema = new Schema({
+//     displayName: String,
+//     username: String,
+//     emailAddress: String
+// },{
+//     timestamps:true,
+//     collection: 'profiles'
+// });
+
+// UserSchema.plugin(passportLocalMongoose);
+
+// export default mongoose.model('Profile', ProfileSchema);
